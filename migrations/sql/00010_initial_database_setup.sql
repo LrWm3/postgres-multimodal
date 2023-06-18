f81da9fb18e43dde8e833d5471ba8583a5c2ddb6
LrWm <William.Marsman@gmail.com>
2023-06-18 13:02:21 -0300

    wip: add '00010_initial_database_setup.sql' migration script

---
+++ migrations/sql/00010_initial_database_setup.sql
@@ -0,0 +1,31 @@
+----------------
+-- Extensions --
+----------------
+
+-- Geospatial
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+-- Timeseries
+CREATE EXTENSION IF NOT EXISTS timescaledb;
+
+-- Vector
+CREATE EXTENSION IF NOT EXISTS vector;
+
+-- Graph / Cypher
+CREATE EXTENSION IF NOT EXISTS age;
+
+-- Hyper log log
+CREATE EXTENSION IF NOT EXISTS hll;
+
+-- Oldschool uuid, todo, upgrade to allow uuid v6 or v7
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Standard extensions
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-- apache age requires ag_catalog on search path
+SET search_path = ag_catalog, "$user", public;
+
+-- Speed up; see: https://github.com/pgvector/pgvector#exact-search
+SET max_parallel_workers_per_gather = 4;
